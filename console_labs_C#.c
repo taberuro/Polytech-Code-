9bbaadd3dcd8f3fb526e000b447f5efad26c9ea6
taberuro <73286579+taberuro@users.noreply.github.com>
2022-03-24 17:40:29 +0300

    Update console_labs_C#.c

--- console_labs_C#.c
+++ console_labs_C#.c
@@ -44,153 +44,179 @@ catch (FormatException)
 
 base
 объявляем необходимые материалы
- int K;
- int k;
- int M;
- int m;
- int sum1 = 0;
- int maxx = 0;
+
+int K;
+int k;
+int M;
+int m;
+int sum1 = 0;
+int maxx = 0;
 // используем конструкцию try catch,чтобы ловить ошибки на протяжении всего кода
- try
- {
-     Console.Write("Введите кол-во видов материалов: ");
-     K = Convert.ToInt32(Console.ReadLine()); 
-     if (K <= 0)
-     {
-         Console.WriteLine("Неверный формат ввода. Введите числa числа больше 0");
-     }
-     
- }
- catch (FormatException)
- {
-     Console.WriteLine("Неверный формат ввода. Введите числa");
-     throw;
- }
- 
- 
- try
- {
-     Console.Write("Введите кол-во видов деталей: ");
-     M = Convert.ToInt32(Console.ReadLine());
-     if (M <= 0)
-     {
-         Console.WriteLine("Неверный формат ввода. Введите числa числа больше 0");
-     }
-
-    
- }
- catch (FormatException)
- {
-     Console.WriteLine("Неверный формат ввода. Введите числa");
-     throw;
- }
- 
-
-     
-     int[,] array = new int[K, M];
-     Random random = new Random();
-     for (int i = 0; i < K; i++)
-     {
-         for (int j = 0; j < M; j++)
-         {
-             array[i, j] = random.Next(100);
-             Console.Write("{0,4}", array[i, j]);
-         }
-         Console.WriteLine();
-     }
-     // part a Для любого материала определить среднюю величину расхода.
- 
- 
-    
- 
- try
- {
-     Console.Write("Введите номер материала: ");
-     k = Convert.ToInt32(Console.ReadLine()) - 1;
-     if (k <= 0)
-     {
-         Console.WriteLine("Неверный формат ввода. Введите числa числа больше 0");
-     }
- }
- catch (FormatException)
- {
-     Console.WriteLine("Неверный формат ввода. Введите числa");
-     throw;
- }
-
- 
-     for (int c = 0; c < M ; c++)
-     {
-         sum1 = sum1 + array[k, c];
-     }
-     sum1 = sum1 / M;
-     Console.Write("Cредняя величина расхода равна " + sum1 );    
-     Console.WriteLine();
-     // part b Для любой детали определить наиболее расходуемый материал.
- 
- try
- {
-     Console.Write("Введите номер детали: ");
-     m = Convert.ToInt32(Console.ReadLine()) - 1; 
-     if (m <= 0)
-     {
-         Console.WriteLine("Неверный формат ввода. Введите числa числа больше 0");
-     }
- }
- catch (FormatException)
- {
-     Console.WriteLine("Неверный формат ввода. Введите числa");
-     throw;
- }
-
- 
-    
-     for (int v = 0; v < K ; v++)
-     {
-         if (array[v, m] > maxx)
-         {
-             maxx = array[v, m];
-         }
-     }
-     Console.Write("наиболее расходуемый материал для выбранной детали " + maxx );    
-     Console.WriteLine();
- 
-
-
-
-
-ПРАКТИКА 1 ( калькулятор ) 
 try
 {
-    double x;
-    double y;
-    string z;
-    Console.Write("Введите первое число:");
-    x = Convert.ToDouble(Console.ReadLine());
-    Console.Write("Введите знак:");
-    z = Console.ReadLine();
-    Console.Write("Введите второе число:");
-    y = Convert.ToDouble(Console.ReadLine());
-    if (z == "+")
+    Console.Write("Введите кол-во видов материалов: ");
+    K = Convert.ToInt32(Console.ReadLine());
+    if (K <= 0)
     {
-        Console.Write(x + y);
-    }
-    if (z == "-")
-    {
-        Console.Write(x - y);
-    }
-    if (z == "%")
-    {
-        Console.Write(x % y);
-    }
-    if (z == "*")
-    {
-        Console.Write(x * y);
+        Console.WriteLine("Неверный формат ввода. Введите числa числа больше 0");
     }
 }
-
 catch (FormatException)
 {
     Console.WriteLine("Неверный формат ввода. Введите числa");
     throw;
+}
+
+
+try
+{
+    Console.Write("Введите кол-во видов деталей: ");
+    M = Convert.ToInt32(Console.ReadLine());
+    if (M <= 0)
+    {
+        Console.WriteLine("Неверный формат ввода. Введите числa числа больше 0");
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Неверный формат ввода. Введите числa");
+    throw;
+}
+
+
+int[,] array = new int[K, M];
+Random random = new Random();
+for (int i = 0; i < K; i++)
+{
+    for (int j = 0; j < M; j++)
+    {
+        array[i, j] = random.Next(100);
+        Console.Write("{0,4}", array[i, j]);
+    }
+
+    Console.WriteLine();
+}
+// part a Для любого материала определить среднюю величину расхода.
+
+
+try
+{
+    Console.Write("Введите номер материала: ");
+    k = Convert.ToInt32(Console.ReadLine()) - 1;
+    if (k <= 0)
+    {
+        Console.WriteLine("Неверный формат ввода. Введите числa числа больше 0");
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Неверный формат ввода. Введите числa");
+    throw;
+}
+
+
+for (int c = 0; c < M; c++)
+{
+    sum1 = sum1 + array[k, c];
+}
+
+sum1 = sum1 / M;
+Console.Write("Cредняя величина расхода равна " + sum1);
+Console.WriteLine();
+// part b Для любой детали определить наиболее расходуемый материал.
+
+try
+{
+    Console.Write("Введите номер детали: ");
+    m = Convert.ToInt32(Console.ReadLine()) - 1;
+    if (m <= 0)
+    {
+        Console.WriteLine("Неверный формат ввода. Введите числa числа больше 0");
+    }
+}
+catch (FormatException)
+{
+    Console.WriteLine("Неверный формат ввода. Введите числa");
+    throw;
+}
+
+
+for (int v = 0; v < K; v++)
+{
+    if (array[v, m] > maxx)
+    {
+        maxx = array[v, m];
+    }
+}
+
+Console.Write("наиболее расходуемый материал для выбранной детали " + maxx);
+Console.WriteLine();
+
+
+
+ПРАКТИКА 1 ( калькулятор ) 
+
+double x;
+double y;
+string z;
+try
+{
+    Console.Write("Введите первое число:");
+    x = Convert.ToDouble(Console.ReadLine());
+}
+catch (FormatException)
+{
+    Console.WriteLine("Неверный формат ввода. Введите первое число правильно");
+    throw;
+}
+try
+{
+    Console.Write("Введите знак:");
+    z = Console.ReadLine();
+}
+catch (FormatException)
+{
+    Console.WriteLine("Неверный формат ввода. Введите знак правильно");
+    throw;
+}
+try
+{
+    Console.Write("Введите второе число:");
+    y = Convert.ToDouble(Console.ReadLine());
+}
+catch (FormatException)
+{
+    Console.WriteLine("Неверный формат ввода. Введите второе число правильно");
+    throw;
+}
+
+
+
+
+if (z == "+")
+{
+    Console.Write(x + y);
+}
+if (z == "-")
+{
+    Console.Write(x - y);
+}
+if (z == "%")
+{
+    if (y==0){Console.Write("деление на ноль запрещено");}
+    else if (y!=0){Console.Write(x % y);}
+}
+if (z == "*")
+{
+    Console.Write(x * y);
+}
+int i, j;
+i = 5;
+j = 5;
+int[,] array = new int[i, j];
+Random random = new Random();
+for (int m = 0; m < i; m++)
+{
+    for (int k = 0; k < j; k++) { array[m, k] = random.Next(100); Console.Write("{0,4}", array[m, k]);}
+    Console.WriteLine();
 }
